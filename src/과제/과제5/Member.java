494f88d5fbbbc84332a0291c3ab4e2cbfe884e6f
qudfoqjf <c8571@naver.com>
2024-01-16 18:00:49 +0900

    Changes

---
+++ src/과제/과제5/Member.java
@@ -0,0 +1,20 @@
+package 과제.과제5;
+
+
+public class Member {
+    String id;
+    String pw;
+    String name;
+    String num;
+    String age;
+
+
+    Member(String id, String pw,String name,String num, String age){
+        this.id=id;
+        this.pw=pw;
+        this.name=name;
+        this.num=num;
+        this.age=age;
+    }
+
+}
